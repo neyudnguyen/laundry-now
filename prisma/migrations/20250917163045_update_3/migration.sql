fae47eb417d760b2b5e2e12d1514684bd6fb0be0
Duyên <duyen0602tm@gmail.com>
2025-09-17 23:30:57 +0700

    refactor: remove bank_account field from VendorProfile model and corresponding migration

---
+++ prisma/migrations/20250917163045_update_3/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `bank_account` on the `vendor_profiles` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."vendor_profiles" DROP COLUMN "bank_account";
