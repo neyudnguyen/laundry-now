ffa9e5af973fd3be8d810d4172bd3a16fd7b90d2
Duyên <duyen0602tm@gmail.com>
2025-09-23 14:56:27 +0700

    feat: update order statuses in schema and migration for improved order management

---
+++ prisma/migrations/20250923075307_update_9/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - The values [pending,accepted,in_progress,need_payment] on the enum `order_statuses` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "public"."order_statuses_new" AS ENUM ('pending_confirmation', 'confirmed', 'picked_up', 'in_washing', 'payment_required', 'completed', 'cancelled');
+ALTER TABLE "public"."orders" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "public"."orders" ALTER COLUMN "status" TYPE "public"."order_statuses_new" USING ("status"::text::"public"."order_statuses_new");
+ALTER TYPE "public"."order_statuses" RENAME TO "order_statuses_old";
+ALTER TYPE "public"."order_statuses_new" RENAME TO "order_statuses";
+DROP TYPE "public"."order_statuses_old";
+ALTER TABLE "public"."orders" ALTER COLUMN "status" SET DEFAULT 'pending_confirmation';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "public"."orders" ALTER COLUMN "status" SET DEFAULT 'pending_confirmation';
