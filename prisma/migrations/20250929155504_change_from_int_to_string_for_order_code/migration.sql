29253bb5cb72657b7060528c767d832d0538f1fa
Duyên <duyen0602tm@gmail.com>
2025-09-29 22:55:20 +0700

    refactor: change order_code type from Int to String in orders and vendor_premium_packages models

---
+++ prisma/migrations/20250929155504_change_from_int_to_string_for_order_code/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "public"."orders" ALTER COLUMN "order_code" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."vendor_premium_packages" ALTER COLUMN "order_code" SET DATA TYPE TEXT;
