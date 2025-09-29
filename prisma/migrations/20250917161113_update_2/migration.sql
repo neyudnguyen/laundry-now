9a4daf0efbd0b632c88fdbc45372fbf253345d6d
Duyên <duyen0602tm@gmail.com>
2025-09-29 19:46:02 +0700

    update: obsolete tables and columns, update foreign key constraints, and introduce new tables for orders, reviews, and premium packages.

--- prisma/migrations/20250917161113_update_2/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."vendor_profiles" DROP CONSTRAINT "vendor_profiles_address_id_fkey";
-
--- AlterTable
-ALTER TABLE "public"."vendor_profiles" ALTER COLUMN "address_id" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "public"."vendor_profiles" ADD CONSTRAINT "vendor_profiles_address_id_fkey" FOREIGN KEY ("address_id") REFERENCES "public"."addresses"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20250917163045_update_3/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `bank_account` on the `vendor_profiles` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."vendor_profiles" DROP COLUMN "bank_account";
